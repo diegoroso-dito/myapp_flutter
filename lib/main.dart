36255b6487af50f54a534b606d8ba41c6aafe171
Diego Roso <diego.silva@dito.com.br>
2023-09-26 18:18:07 -0300

    change layout of home page

--- lib/main.dart
+++ lib/main.dart
@@ -16,80 +16,62 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      title: 'MyApp - DitoSDK',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('MyApp - DitoSDK'),
+          backgroundColor: Colors.blue,
+        ),
+        body: const MyHomePage(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  void _identify() {
-    DitoSDK().identify(
-      cpf: '43536713831',
-      name: "Diego Roso",
-      email: "diego.silva@dito.com.br",
-      gender: "male",
-      birthday: "1995-07-20",
-      location: "Piracaia-SP",
-      customData: {'Attr2': 'Attr2Value'},
-    );
-  }
-
-  void _register() {
-    DitoSDK().registerUser();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '0',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          FloatingActionButton(
-            onPressed: _identify,
-            tooltip: 'Decrement',
-            child: const Text(
-              'setUser',
-            ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          OutlinedButton(
+            onPressed: () {
+              DitoSDK().identify(
+                cpf: '43536713831',
+                name: "Diego Roso",
+                email: "diego.silva@dito.com.br",
+                gender: "male",
+                birthday: "1995-07-20",
+                location: "Piracaia-SP",
+                customData: {'Cargo': 'Tech Lead'},
+              );
+            },
+            child: const Text('Identify User'),
           ),
-          const SizedBox(width: 16),
-          FloatingActionButton(
-            onPressed: _register,
-            tooltip: 'Increment',
-            child: const Text(
-              'Tracker',
-            ),
+          const SizedBox(height: 20),
+          OutlinedButton(
+            onPressed: () {
+              DitoSDK().registerUser();
+            },
+            child: const Text('Register User'),
+          ),
+          const SizedBox(height: 20),
+          OutlinedButton(
+            onPressed: () {
+              DitoSDK().trackEvent(
+                  eventName: 'comprou',
+                  revenue: 99.00,
+                  properties: {'produto': 'tenis'});
+            },
+            child: const Text('Track'),
           ),
         ],
       ),
